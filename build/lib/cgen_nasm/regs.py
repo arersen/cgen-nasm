d051e6bc722cbfc856499a7371461cf1b926e23c
baddev <38311102+qbaddev@users.noreply.github.com>
2020-04-10 13:59:12 +0300

    *

---
+++ build/lib/cgen_nasm/regs.py
@@ -0,0 +1,28 @@
+class Regs:
+    # 16 bit
+    AX = 'AX'
+    BX = 'BX'
+    CX = 'CX'
+    DX = 'DX'
+    BP = 'BP'
+    SI = 'SI'
+    DI = 'DI'
+    SP = 'SP'
+
+    # 32 bit
+    EAX = 'EAX'
+    EBX = 'EBX'
+    ECX = 'ECX'
+    EDX = 'EDX'
+    EBP = 'EBP'
+    ESI = 'ESI'
+    EDI = 'EDI'
+    ESP = 'ESP'
+
+    # Segments regs
+    SS = 'SS'
+    CS = 'CS'
+    DS = 'DS'
+    ES = 'ES'
+    FS = 'FS'
+    GS = 'GS'
